c291b6777669fb4bcd03977b071b95a91210cbf6
Paul Rosham <prosham@purestorage.com>
2024-12-19 16:04:11 +1100

    successfully adding tage - not default correctly thoug

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Populate a sample Fusion environment with Realms, workloads and host and try to porpogate tags into Pure1 for customised reporting
+STAAS-Reporting-Fusion
+
+This package will add chargeback metadata to block volumes on a fleet of Pure Storage FlashArrays, and then provide regular space reporting for chargeback.
+
+It depends on some external packages:
+    pip install pandas
+    pip install openpyxl
+    pip install py-pure-client
+
+The minimum Purity//FA Rest API version is 2.38 for the fusion API
+
+API Tokens are stored with other Global Variables in the 
--- staas-tag_vols.py
+++ staas-tag_vols.py
@@ -1,43 +1,41 @@
 import pandas as pd
 import pypureclient
 import urllib3
+import re
 
 from pypureclient import flasharray
 from pypureclient.flasharray import Client, PureError
-from pypureclient.client_settings import get_client_versions
+#from pypureclient.client_settings import get_client_versions
 
 
-debug=1
+debug=2
 
-# Function to find and tag volumes
-def tag_volumes(client, arrays_name, app_name, num_volumes, size_volumes):
-    for i in range(num_volumes):
-        volume_name = f"{pod_name}::{workload_name}::volume{i+1}"
-        
-        
-        try:
-            response = client.create_volume(name=volume_name, size=size_volumes)
-            if isinstance(response, PureError):
-                print(f"Error creating volume {volume_name}: {response}")
-            else:
-                print(f"Volume {volume_name} created successfully")
-        except PureError as e:
-            print(f"Error creating volume {volume_name}: {e}")
-                # Add the tag to the volumes using the correct method
-        
-        for volume in volumes:
-            response = client.put_volumes_tags_batch(resources={volume.name}, tag=tags)
-            # Check the response
-            if response.status_code == 200:
+# Find and tag all volumes, with a correct chargeback code if defined, or the default IT code otherwise
+def process_volumes(fleet_member):
+    response = client.get_volumes(context_names=fleet_member.member.name)
+    if response.status_code == 200:
+            if debug >1:
+                print(f"finding volumes for array {fleet_member.member.name}")
+            volumes=response.items
+    else:
+        print(f"Failed to retrieve volumes. Status code: {response.status_code}, Error: {response.errors}")
+        return()
+    if debug >= 2:
+            print(f"Array,Realm,Volume,Pod")
+    for volume in volumes:
+        if debug >= 2: 
+            print(f"{fleet_member.member.name},{volume.name},{volume.pod}")
+        response = client.put_volumes_tags_batch(context_names=fleet_member.member.name, resource_names=volume.name, tag=tags)
+        # Check the response
+        if response.status_code == 200:
+            if debug >=1:
                 print(f"Tags added successfully to volume {volume.name}.")
-            else:
-                print(f"Failed to add tags to {volume.name}. Status code: {response.status_code}, Error: {response.errors}")
-
-
+        else:
+            print(f"Failed to add tags to {volume.name}. Status code: {response.status_code}, Error: {response.errors}")
 
 
 def list_fleets():
-    # Retrieve the list of fleet, then find all of the FlashArrays and volumes associated with the fleet
+    # Retrieve the list of fleets, then find all of the FlashArrays and volumes associated with the fleet
     response = client.get_fleets_members()
     if response.status_code == 200:
         fleets_members=response.items
@@ -45,42 +43,79 @@ def list_fleets():
         print(f"Failed to retrieve fleets/members. Status code: {response.status_code}, Error: {response.errors}")
     return(fleets_members)
 
-API_TOKEN="5887696c-bceb-1ea1-77df-f316fb18c090"
-FUSION_SERVER="pstg-fa-02.mel.aulab.purestorage.com"
-#FUSION_SERVER="10.111.0.5"
-USER_NAME="prosham"
+# Check API version
+def check_api_version(level):
+    version = client.get_rest_version()
+    
+    if float(version) >= level:
+        return True
+    else:
+        if debug >= 1:
+            print(f'API Version: {version}')
+            print(f"API version needs to support Fusion v2 at a minimum.")
+        return False
+    
+# Function to check admin level
+def check_admin_level(desired_role):
+    try:
+        response = client.get_admins()
+        if response.status_code == 200:
+            admins = response.items
+            for admin in admins:
+                if admin.name == USER_NAME:
+                    if debug >= 1:
+                        print(f'User {USER_NAME} has admin level: {admin.role}')
+                    if admin.role == desired_role:
+                        return True
+                    else:
+                        print(f'User {USER_NAME} does not have admin privileges.')
+                        return True
+        else:
+            print(f'Failed to get admins: {response.errors}')
+    except PureError as e:
+        print(f'Error checking admin level: {e}')
+
+USER_NAME=""
+TAG_API_TOKEN=""
+FUSION_SERVER=""
+NAMESPACE=""
 
 # Disable certificate warnings
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Main script
 if __name__ == "__main__":
+    # Read the Excel file
+    spreadsheet = pd.ExcelFile('STAAS_Tagging.xlsx')
+
+    # Parse the specific sheet
+    fleet_df = spreadsheet.parse('Fleet')
+
+    # Extract global variables
+    global_variables = fleet_df.iloc[0].to_dict()
+
+    # Assign global variables
+    USER_NAME = global_variables.get('USER_NAME', '')
+    API_TOKEN = global_variables.get('API_TOKEN', '')
+    FUSION_SERVER = global_variables.get('FUSION_SERVER', '')
+    NAMESPACE = global_variables.get('NAMESPACE', '')
+
     try:
         # Initialize the client
         client = Client(FUSION_SERVER,username=USER_NAME, api_token=API_TOKEN)
-        # Check to see minimum version of 2.37
-        response = client.get_target_versions()
-        # Check if the API version is at least 2.37
-        #required_version = '2.37'
-        #if api_version >= required_version:
-        #    print(f"API version {api_version} is valid.")
-        #else:
-        #    raise ValueError(f"API version {api_version} is not supported. Minimum required version is {required_version}.")
-        if response.status_code == 200:
-            versions=response.items
-        else:
-            print(f"Failed to retrieve fleets/members. Status code: {response.status_code}, Error: {response.errors}")
+        # Check to see minimum version of 2.38 & array admin privileges for this user
+        if not check_admin_level("array_admin"):
+            exit(1)
+        if not check_api_version(2.38):
+            exit(2)
         
         tags = [
-            {"key": "chargeback", "value": "App1", "namespace": "Telstra-STAAS"},
+            {"key": "chargeback", "value": "IT", "namespace": "NAMESPACE"},
         ]
         
         # Get the arrays for reporting contexts for the nominated fleet
-        arrays=list_fleets()
-        
-        for array in arrays:
-            print(f"Array {array.member.name} is in fleet {array.fleet.name}")
-        # Tag all volumes
-        #tag_volumes(volumes,tags)
+        for member in list_fleets():
+            # Tag all volumes in pods for arrays in the fleet
+            process_volumes(member)
 
     except PureError as e:
         print(f"Error initializing client: {e}")
